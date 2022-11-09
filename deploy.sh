8a2b40f77e9c4d2aec86383d7e920d9cef0980d6
unknown <kirubelfikruwsh@gmail.com>
2022-11-09 10:53:51 +0300

    Updated HPA and Backend Logs

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,14 @@ kubectl apply -f feed-deploy.yml
 kubectl apply -f user-deploy.yml
 kubectl apply -f reverseproxy-deploy.yml
 kubectl apply -f frontend-deploy.yml
+
+kubectl apply -f aws-creds-configmap.yaml
+kubectl apply -f env-configmap.yaml
+kubectl apply -f env-secret.yaml
+
+kubectl apply -f feed-service.yaml
+kubectl apply -f user-service.yaml
+kubectl apply -f reverseproxy-service.yaml
+kubectl apply -f frontend-service.yaml
+
+
